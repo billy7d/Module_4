4e72ebe2faaab4d8939c9bff4e3066eda703fab5
Nguyen Hung <billy7d95@gmail.com>
2020-10-13 23:21:10 +0700

    add

---
+++ Week 2/validating_fom_input/src/main/java/controller/UserController.java
@@ -0,0 +1,31 @@
+package controller;
+
+
+import model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user")
+    public String showForm(Model model) {
+    model.addAttribute("user",new User());
+    return "index";
+    }
+
+    @PostMapping("/validateUser")
+    public String checkValidation(@Validated @ModelAttribute("user") User user, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "index";
+        } else {
+            return "result";
+        }
+    }
+}
+
